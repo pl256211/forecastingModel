aac027afe5012a3c3785d216191a33c34fe1e495
Anik Bora <anikbora@aniks-mbp.lan>
2022-07-07 22:22:21 -0400

    Created USDM Data collection script and rearranged file structure

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Find a relation between snowpack water value and droughts
 to extract weather and drought data from google earth engine.
 ## How to Run
 ```
-python daymet_data_script.py
+python ./dataDownloader/daymet_data_script.py
 ```
 ## Prerequsites
 Google Earth Engine and JSON Modules
@@ -14,3 +14,22 @@ Google Earth Engine and JSON Modules
 ## Output
 Produces Multiple JSON files containing weather and drought data for California
 The file name will be in the format of "{band}/rawCaliforniaWeatherData{yyyy}.json". Band will tell the type of the data.
+
+# USDM_data_extraction
+
+## Purpose
+To extract USDM drought data shapefiles and store them neatly in files
+
+## Prerequsites
+Requests
+
+## How to Run
+```
+python ./dataDownloader/USDM_data_extraction.py
+
+```
+
+## Output
+Produces multiple zip files with path "./dataDownloader/dataStorage/raw"
+Produces multiple zip files with path "./dataDownloader/dataStorage/temp"
+Produces multiple shp files with path "./dataDownloader/dataStorage/unzipped
---
+++ dataDownloader/USDM_data_extraction.py
@@ -0,0 +1,59 @@
+# importing the requests module
+import requests
+#import the ability to change zip files
+from zipfile import ZipFile
+#import the ability to make changes to files
+import os
+
+def download_file(url, directory):
+    local_filename = url.split('/')[-1]
+    # NOTE the stream=True parameter below
+    with requests.get(url) as r:
+        r.raise_for_status()
+        with open(directory + local_filename, 'wb') as f:
+            f.write(r.content)
+    return local_filename
+
+
+def pullData(years, year):
+    for i in range (years):
+        url = 'https://droughtmonitor.unl.edu/data/shapefiles_m//' + str(year) + '_USDM_M.zip'
+
+        # Downloading the file by sending the request to the URL
+        #req = requests.get(url)
+        
+        # Split URL to get the file name
+        #filename = url.split('/')[-1]
+        #print(filename)
+        
+        directoryName = './dataStorage/raw/'
+
+        if not os.path.exists(directoryName):
+            os.mkdir(directoryName)
+
+        filename = download_file(url, directoryName)
+        print(filename)
+
+        # Writing the file to the local file system
+        #with open(directoryName + filename,'wb') as output_file:
+         #   output_file.write(req.content)
+        print('Downloading Completed')
+
+        with ZipFile(directoryName + filename, 'r') as zip:
+            zip.extractall('./dataStorage/temp/' + str(year))
+
+        
+        for path in os.listdir('./dataStorage/temp/' + str(year)):
+            with ZipFile("./dataStorage/temp/" + str(year) + '/' + path, 'r') as zip:
+                zip.extractall('./dataStorage/unzipped/' + str(year))
+        year -=1
+
+def removeEverythingButSHP(years, startYear):
+    year = startYear
+    for i in range(years):
+        for path in os.listdir('./dataStorage/unzipped/' + str(year)):
+            if path[-3:] != 'shp':
+               os.remove('./dataStorage/unzipped/' + str(year) + '/' + path)
+        year -= 1
+pullData(10, 2022)
+removeEverythingButSHP(10, 2022)
