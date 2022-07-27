9f521b24ae82ef76f9fc430e663591f40ff6af90
Anik Bora <anikbora@Aniks-MacBook-Pro.local>
2022-07-27 10:50:16 -0400

    added a mask channel to each numpy array that is produced

--- data_proccessing/USDM_rasterization.py
+++ data_proccessing/USDM_rasterization.py
@@ -6,70 +6,91 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 from geocube.api.core import make_geocube
 import os
+import json
+from shapely.geometry import box
 
 mainDir = '/Volumes/AnikSchool/research_project/forecastingModel/dataDownloader/dataStorage/'
 
-def produceOutGrid(file):
-    x = geopandas.read_file(file)
-    x['value'] = x['DM']
 
-
-
+US = geopandas.read_file(mainDir + 'gadm36_USA_shp/gadm36_USA_0.shp')
+US['value'] = 0
+print(US)
+def printVector(v):
+    
     out_grid = make_geocube(
-        vector_data=x,
+        vector_data=v,
         measurements = ['value'],
-        resolution=(-0.01, 0.01),
+        resolution=(-0.05, 0.05),
         fill=-9999
     )
     return out_grid
+
+def produceOutGrid(file):
+    x = geopandas.read_file(file)
+    x['value'] = x['DM'] + 1
+    print(x)
+    newX = US.append(x)
+    print(newX)
+
+
+    out_grid = make_geocube(
+        vector_data=newX,
+        measurements = ['value'],
+        resolution=(-0.05, 0.05),
+        fill=-9999
+        #geom=json.dumps(geom)
+    )
+    print(out_grid.dims)
+    new = out_grid.to_array()
+    new = new.to_numpy()
+    mask = xr.where(out_grid.value<-1999.0, 0, 1)
+    mask = mask.to_numpy()
+    masked = np.append([mask], new, axis = 0)
+    print(type(masked))
+    print(masked.shape)
+    print(masked)
+    return out_grid, masked
     
 def rasterize(makeFile=False):
     years = os.listdir(mainDir + 'temp')
-    years.remove('._.DS_Store')
-    years.remove('.DS_Store')
+    try:
+        years.remove('._.DS_Store')
+        years.remove('.DS_Store')
+    except:
+        pass
     for i in years:
         files = os.listdir(mainDir + 'temp/' + i)
         for j in files:
-            out_grid = produceOutGrid(mainDir + 'temp/' + i + '/' + j)
-            '''x = geopandas.read_file(mainDir + 'temp/' + i + '/' + j)
-            x['value'] = x['DM']
-
-
-            print(x)
-
-            out_grid = make_geocube(
-                vector_data=x,
-                measurements = ['value'],
-                resolution=(-0.01, 0.01),
-                fill=-9999
-            )'''
+            out_grid = produceOutGrid(mainDir + 'temp/' + i + '/' + j)[1]
+            if not os.path.exists(mainDir + 'rasterized/'):
+                os.mkdir(mainDir + 'rasterized/' )
             if not os.path.exists(mainDir + 'rasterized/' + i):
                 os.mkdir(mainDir + 'rasterized/' + i)
             if makeFile == True:
-                out_grid.rio.to_raster(mainDir + 'rasterized/' + i + j[:14] + '.tiff')
-#print(os.listdir('/Volumes/AnikSchool/research_project/forecastingModel/dataDownloader/dataStorage/temp'))
-#print(out_grid)
-#print(out_grid.value)
+                np.save(mainDir + 'rasterized/' + i + '/' + j[:14] + '.npy',out_grid)
 
-#print(out_grid['value'])
-
-
-#print(out_grid2)
-
-#print(out_grid_norm)
 def show(out_grid):
     fig = plt.figure(figsize=(17, 15))
     ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
     da_grib = xr.where(out_grid.value<-1999.0, np.nan, out_grid.value)
+    #print(type(da_grib))
+    #print(da_grib)
+    new = type(da_grib.to_numpy())
+   
 
-    da_grib.plot(levels = [0,1,2,3,4,5])
+    #da_grib2 = xr.where(out_grid2.value<-1999.0, np.nan, out_grid2.value)
+    
+    #da_grib2.plot(levels = [0,1,2,3,4,5,6])
+    da_grib.plot(levels = [0, 1, 2, 3, 4, 5, 6])
 
 
     ax.set_extent([-140, -50, 10, 60])
     ax.set_title("Rasterized with 0.01$^o$ Grids", fontsize=24)
     plt.show()
 
-x = produceOutGrid('/Volumes/AnikSchool/research_project/forecastingModel/dataDownloader/dataStorage/temp/2018/USDM_20181113_M.zip')
+print(type(US))
+x = produceOutGrid('/Volumes/AnikSchool/research_project/forecastingModel/dataDownloader/dataStorage/temp/2022/USDM_20220104_M.zip')[0]
+rasterize(True)
 show(x)
 
 
--- data_proccessing/daymet_masking.py
+++ data_proccessing/daymet_masking.py
@@ -5,17 +5,26 @@ import numpy.ma as ma
 from numpy import save
 
 filePath = '/Volumes/AnikSchool/research_project/forecastingModel/dataDownloader/dataStorage/raw/daymet/'
-print(filePath[:-11])
 def mask(dir):
     for i in os.listdir(dir):
         ds = nc.Dataset(str(dir + i))
         band = i[19:-8]
-        #print(band)
-        if not os.path.exists(filePath[:-11] + '/masked/'):
-            os.mkdir(filePath[:-11] + '/masked/')
-        if not os.path.exists(filePath[:-11] + '/masked/daymet/'):
-            os.mkdir(filePath[:-11] + '/masked/daymet/')
-        tmax = ds[band][:]
-        save(filePath[:-11] + '/masked/daymet/' + i + '.npy',tmax)
+        year = i[-7:-3]
+        print(year)
+        for j in range(len(ds[band])):
+            if not os.path.exists(filePath[:-11] + 'masked/'):
+                os.mkdir(filePath[:-11] + 'masked/')
+            if not os.path.exists(filePath[:-11] + 'masked/daymet/'):
+                os.mkdir(filePath[:-11] + 'masked/daymet/')
+            if not os.path.exists(filePath[:-11] + 'masked/daymet/' + year + '/'):
+                os.mkdir(filePath[:-11] + 'masked/daymet/' + year + '/')
+            if not os.path.exists(filePath[:-11] + 'masked/daymet/' + year + '/' + i + '/'):
+                os.mkdir(filePath[:-11] + 'masked/daymet/' + year + '/' + i + '/')
+            tmax = ds[band][j][:]
+            tmax = tmax.filled(-9999)
+            mask = np.where(tmax<-1999.0, 0, 1)
+            
+            masked = np.append([mask], [tmax], axis = 0)
+            np.save(filePath[:-11] + 'masked/daymet/' + year + '/' + i + '/'+  str(j) + '.npy',masked)
 
 mask(filePath)
